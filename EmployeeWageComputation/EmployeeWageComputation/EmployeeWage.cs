e79b12710f1c5c0175f2208b50fa32ee90370348
“VenuReddy773” <venu706@gmail.com>
2021-11-09 19:50:07 +0530

    [VenuGopalReddy] : Added Part Time Employee and Wage Feature

--- EmployeeWageComputation/EmployeeWageComputation/EmployeeWage.cs
+++ EmployeeWageComputation/EmployeeWageComputation/EmployeeWage.cs
@@ -5,20 +5,24 @@ namespace EmployeeWageComputation
 {
     class EmployeeWage
     {
-        public const int IS_PRESENT = 1,WAGE_PER_HR = 20, FULL_WORKING_HRS=8;
+        public const int IS_FULL_TIME = 1,IS_PART_TIME = 2, WAGE_PER_HR = 20, FULL_WORKING_HRS=8, PARTTIME_WORKING_HRS = 4;
         int TotalEmpWage;
         public void DailyWage()
         {
             Random random = new Random();
-            int empcheck = random.Next(0, 2);
-            if (empcheck == IS_PRESENT)
+            int empcheck = random.Next(0, 3);
+            if (empcheck == IS_FULL_TIME)
             {
                 this.TotalEmpWage = FULL_WORKING_HRS * WAGE_PER_HR;
                 Console.WriteLine("Employee Present and Daily Wage is: " + TotalEmpWage );
+            }          
+            else if(empcheck == IS_PART_TIME)
+            {
+                this.TotalEmpWage = PARTTIME_WORKING_HRS * WAGE_PER_HR;
+                Console.WriteLine("Employee Present and Daily Wage is: " + TotalEmpWage);
             }
-                
             else
-                Console.WriteLine("Employee Absent");
+            Console.WriteLine("Employee Absent");
         }        
     }
 }
